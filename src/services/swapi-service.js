b33d4cad6a5a9ec92560460d8e2de7c064e04134
Anna Lee <annaleeart@yandex.ru>
2021-02-20 22:10:56 +0300

    basic project structure

---
+++ src/services/swapi-service.js
@@ -0,0 +1,50 @@
+// https://swapi.dev
+
+export default class SwapiService {
+    // это приватная часть класса, её не следует использовать и изменять снаружи
+    _apiBase = 'https://swapi.dev/api';
+
+    async getResource(url) {
+        const res = await fetch(`${this._apiBase}${url}`);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}` +
+            `, received ${res.status}`)
+        }
+    
+        return await res.json();
+    }
+
+    async getAllPeople() {
+        const res = await this.getResource(`/people/`);
+        return res.results;
+    }
+
+    getPerson(id) {
+        return this.getResource(`/people/${id}/`);
+    }
+
+    async getAllPlanets() {
+        const res = await this.getResource(`/planets/`);
+        return res.results;
+    }
+
+    getPlanet(id) {
+        return this.getResource(`/planets/${id}/`);
+    }
+
+    async getAllStarships() {
+        const res = await this.getResource(`/starships/`);
+        return res.results;
+    }
+
+    getStarship(id) {
+        return this.getResource(`/starships/${id}/`);
+    }
+}
+// запускаем код
+const swapi = new SwapiService(); 
+// запрашиваем всех людей
+swapi.getPerson(3).then((p) => {
+        console.log(p.name);
+});
