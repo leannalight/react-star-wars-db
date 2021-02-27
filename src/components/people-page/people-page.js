721467279e5490d3bd57f0f8e6e819fb3771f6b6
Anna Lee <annaleeart@yandex.ru>
2021-02-27 20:17:46 +0300

    react elements as values of properties

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -7,6 +7,19 @@ import ErrorIndicator from '../error-indicator/error-indicator';
 import './people-page.css';
 import SwapiService from '../../services/swapi-service';
 
+const Row = ({ left, right }) => {
+  return (
+    <div className="row mb2">
+      <div className="col-md-6">
+        {left}
+      </div>
+      <div className="col-md-6">
+        {right}
+      </div>
+    </div>
+  )
+}
+
 export default class PeoplePage extends Component {
 
   swapiService = new SwapiService(); // подключаем к серверу
@@ -33,20 +46,22 @@ export default class PeoplePage extends Component {
       return <ErrorIndicator />;
     }
 
+    const itemList = ( // вынесли отдельно
+      <ItemList
+        onItemSelected={this.onPersonSelected}
+        getData={this.swapiService.getAllPeople}
+        renderItem={({name, gender, birthYear}) => (
+          `${name} (${gender}, ${birthYear})`)}  />
+    );
+
+    const personDetails = ( // вынесли отдельно
+      <PersonDetails personId={this.state.selectedPerson} />
+    );
+
     return (
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList
-            onItemSelected={this.onPersonSelected}
-            getData={this.swapiService.getAllPeople}
-            renderItem={({name, gender, birthYear}) =>
-              (`${name} (${gender}, ${birthYear})`)}  />   {/* здесь ф-я getAllPeople не вызывается,
-                                                      а передается - тогда компонент может сам решать
-                                                      когда ее вызвать*/}
-        </div>
-        <div className="col-md-6">
-          <PersonDetails personId={this.state.selectedPerson} />
-        </div>
+      <div>
+        <Row left={itemList} right={personDetails} />
+        <Row left="Foo" right="Bar" />
       </div>
     );
   }
